3f37697e8792cbd3e0f706e6150774521b173271
sharmaprashant18 <ps3348644@gmail.com>
2026-04-05 17:13:16 +0545

    Added the khalti in the elearning and the ecommerce with the web view inside the app

--- lib/ecommerce/model/check_out_summary_model.dart
+++ lib/ecommerce/model/check_out_summary_model.dart
@@ -26,54 +26,6 @@ class CheckoutOrderItem {
       );
 }
 
-// class CheckoutSummaryResponse {
-//   final String message;
-//   final String orderId;
-//   final String fullName;
-//   final String address;
-//   final String phoneNumber;
-//   final String paymentType;
-//   final List<CheckoutOrderItem> items;
-//   final int totalItems;
-//   final double totalAmount;
-//   final double shippingCharge;
-//   final double grandTotal;
-//   final String status;
-
-//   const CheckoutSummaryResponse({
-//     required this.message,
-//     required this.orderId,
-//     required this.fullName,
-//     required this.address,
-//     required this.phoneNumber,
-//     required this.paymentType,
-//     required this.items,
-//     required this.totalItems,
-//     required this.totalAmount,
-//     required this.shippingCharge,
-//     required this.grandTotal,
-//     required this.status,
-//   });
-
-//   factory CheckoutSummaryResponse.fromJson(Map<String, dynamic> json) =>
-//       CheckoutSummaryResponse(
-//         message: json['message'] as String,
-//         orderId: json['order_id'] as String,
-//         fullName: json['full_name'] as String,
-//         address: json['address'] as String,
-//         phoneNumber: json['phone_number'] as String,
-//         paymentType: json['payment_type'] as String,
-//         items: (json['items'] as List<dynamic>)
-//             .map((e) =>
-//                 CheckoutOrderItem.fromJson(e as Map<String, dynamic>))
-//             .toList(),
-//         totalItems: json['total_items'] as int,
-//         totalAmount: (json['total_amount'] as num).toDouble(),
-//         shippingCharge: (json['shipping_charge'] as num).toDouble(),
-//         grandTotal: (json['grand_total'] as num).toDouble(),
-//         status: json['status'] as String,
-//       );
-// }
 class CheckoutSummaryResponse {
   final String message;
   final String orderId;
